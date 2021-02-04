5bf7e59a3e0f263f46c99dc1e69677f0df8e0174
jpcanto <jplopessk8@gmail.com>
2021-02-04 20:12:22 -0300

    feat: adding simple validations

---
+++ src/data/cards.ts
@@ -0,0 +1,44 @@
+export const Cards = [
+  {
+    size: 0,
+    title: "size-0",
+    description: "Se abstem da votação.",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+  {
+    size: 1,
+    title: "size-1",
+    description: "Até 1 hora.",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+  {
+    size: 2,
+    title: "size-2",
+    description: "Uma manhã.",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+  {
+    size: 3,
+    title: "size-3",
+    description: "Uma tarde.",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+  {
+    size: 5,
+    title: "size-5",
+    description: "Um dia.",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+  {
+    size: 8,
+    title: "size-8",
+    description: "Até 3 dias",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+  {
+    size: 13,
+    title: "size-13",
+    description: "Até 5 dias",
+    image: require("@/assets/card-size-1.jpg"),
+  },
+];
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,103 +1,77 @@
 <template>
-  <v-card class="mx-auto my-12" max-width="374">
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
+  <v-app>
+    <v-container v-if="!entered">
+      <v-form ref="form">
+        <v-text-field
+          v-model="userName"
+          label="Apelido"
+          required
+        ></v-text-field>
 
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
+        <v-btn color="success" class="mr-4" @click="updateUsername">
+          Submit
+        </v-btn>
+      </v-form>
+    </v-container>
 
-    <v-card-title>Cafe Badilico</v-card-title>
+    <v-container v-else class="grey lighten-5">
+      <v-row>
+        <v-col
+          v-for="(card, index) in cards"
+          :key="index"
+          cols="12"
+          sm="3"
+          @click="updateVote(card.size)"
+        >
+          <v-card class="mx-auto" max-width="200">
+            <v-img contain height="300" :src="card.image"></v-img>
 
-    <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
+            <v-card-title>{{ card.title }}</v-card-title>
 
-        <div class="grey--text ml-4">
-          4.5 (413)
-        </div>
+            <v-card-text>
+              <div>{{ card.description }}</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
-
-      <div class="my-4 subtitle-1">
-        $ • Italian, Cafe
-      </div>
-
-      <div>
-        Small plates, salads sandwiches - an intimate setting with 12 indoor
-        seats plus patio seating.
-      </div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group active-class="deep-purple accent-4 white--text" column>
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn color="deep-purple lighten-2" text>
-        Reserve
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import fire from "../services/firebase";
+import firebase from "../services/firebase";
+import { Cards } from "../data/cards";
 
 export default {
   name: "App",
   data() {
     return {
+      cards: Cards,
+      entered: false,
       userName: "",
-      name: null,
-      showMessage: "",
-      messages: [],
+      poker: { name: null, vote: 0 },
     };
   },
   methods: {
     updateUsername() {
-      this.name = this.userName;
-      console.log(this.userName);
-      this.userName = "";
+      if (this.userName !== "") {
+        this.poker.name = this.userName;
+        this.entered = true;
+        this.userName = "";
+      }
     },
-    sendMessage() {
-      const message = {
-        text: this.showMessage,
-        username: this.name,
-      };
-      fire
+    updateVote(size) {
+      this.poker.vote = size;
+    },
+    sendVote() {
+      firebase
         .database()
-        .ref("messages")
-        .push(message);
-      this.showMessage = "";
+        .ref("poker")
+        .push(this.poker);
     },
   },
   mounted() {
-    const itemsRef = fire.database().ref("messages");
+    const itemsRef = firebase.database().ref("messages");
     itemsRef.on("value", (snapshot) => {
       const data = snapshot.val();
       const messages = [];
@@ -105,7 +79,7 @@ export default {
         messages.push({
           id: key,
           username: data[key].username,
-          text: data[key].text,
+          vote: data[key].vote,
         });
       });
       this.messages = messages;
@@ -113,63 +87,3 @@ export default {
   },
 };
 </script>
-
-<style>
-@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons");
-#app {
-  font-family: "Roboto", sans-serif;
-  font-size: 18px;
-}
-.login {
-  background: #fff;
-  width: 40%;
-  height: 50vh;
-  margin: auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-h3 {
-  font-size: 30px;
-  text-align: center;
-}
-input {
-  width: 100%;
-  border-radius: 4px;
-  border: 1px solid rgb(156, 156, 156);
-  padding-left: 2px;
-  padding-right: 2px;
-}
-.message-body {
-  width: 40%;
-  height: 80vh;
-  margin: auto;
-}
-.message-text {
-  min-width: 10%;
-  border-radius: 4px;
-}
-.message {
-  font-size: 14px;
-}
-.mg-text {
-  color: rgb(0, 195, 255);
-  font-weight: bolder;
-}
-.message-body input {
-  width: 80%;
-  border-radius: 4px;
-  border: 1px solid rgb(156, 156, 156);
-  height: 6vh;
-  padding-left: 2px;
-  padding-right: 2px;
-}
-.card {
-  width: 100%;
-  height: 75vh;
-  margin: auto;
-}
-.card-body {
-  min-height: 50vh;
-  overflow-x: scroll;
-}
-</style>
